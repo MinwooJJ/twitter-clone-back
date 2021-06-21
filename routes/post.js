68067f69c03e00cf7e0507a9bc9ccc3575ca12a1
Minwoo <minwoojung01@gmail.com>
2021-06-21 11:43:16 -0700

    Add post and comment routes

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const { Post, Comment } = require('../models');
+const { isSignedIn } = require('./middlewares');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  res.json('yeah');
+router.post('/', isSignedIn, async (req, res) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id,
+    });
+    res.status(201).json(post);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/:postId/comment', isSignedIn, async (req, res) => {
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.postId },
+    });
+
+    if (!post) {
+      return res
+        .status(403)
+        .send(`The post you were looking for doesn't exist`);
+    }
+
+    const commnet = await Comment.create({
+      content: req.body.content,
+      PostId: req.params.postId,
+      UserId: req.user.id,
+    });
+    res.status(201).json(commnet);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 router.delete('/', (req, res) => {
