86cf6c0e7bd881ac96b67ce1852ce8a83266617d
Minwoo <minwoojung01@gmail.com>
2021-07-12 18:55:34 -0700

    Add helmet, hpp and production mode

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
-const dotenv = require('dotenv');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const dotenv = require("dotenv");
+const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
 
-const postRouter = require('./routes/post');
-const postsRouter = require('./routes/posts');
-const userRouter = require('./routes/user');
-const hashtagRouter = require('./routes/hashtag');
-const db = require('./models');
-const passportConfig = require('./passport');
+const postRouter = require("./routes/post");
+const postsRouter = require("./routes/posts");
+const userRouter = require("./routes/user");
+const hashtagRouter = require("./routes/hashtag");
+const db = require("./models");
+const passportConfig = require("./passport");
 
 // .env 가져오기
 dotenv.config();
@@ -21,25 +23,31 @@ const app = express();
 db.sequelize
   .sync()
   .then(() => {
-    console.log('db is connected!');
+    console.log("db is connected!");
   })
   .catch(console.error);
 
 // passport 실행
 passportConfig();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan("dev"));
+}
 
 // 모든 요청 허용
 app.use(
   cors({
     // ACCESS-CONTROL-ALLOW-ORIGIN, CREDENTIALS
-    origin: 'http://localhost:3000',
+    origin: ["http://localhost:3000", "tweeter.com"],
     credentials: true,
   })
 );
 // MAC, linux의 운영체제에 때문에 .join 사용
-app.use('/', express.static(path.join(__dirname, 'uploads')));
+app.use("/", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -53,19 +61,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('hello express');
+app.get("/", (req, res) => {
+  res.send("hello express");
 });
 
-app.get('/', (req, res) => {
-  res.send('hello api');
+app.get("/", (req, res) => {
+  res.send("hello api");
 });
 
-app.use('/user', userRouter);
-app.use('/post', postRouter);
-app.use('/posts', postsRouter);
-app.use('/hashtag', hashtagRouter);
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+app.use("/posts", postsRouter);
+app.use("/hashtag", hashtagRouter);
 
 app.listen(3065, () => {
-  console.log('Server On!');
+  console.log("Server On!");
 });
